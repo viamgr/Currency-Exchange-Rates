d33a8774d1ce29fd95a99b376ad324fdac8eb45d
Vahid Moradi <zvm4zvm@gmail.com>
2023-01-08 13:22:29 +0330

    #14 Added test runners

---
+++ base_ui/android_test_shared/src/main/java/app/vahid/base_ui/android_test_shared/HiltTestActivity.kt
@@ -0,0 +1,7 @@
+package app.vahid.base_ui.android_test_shared
+
+import androidx.appcompat.app.AppCompatActivity
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HiltTestActivity : AppCompatActivity()

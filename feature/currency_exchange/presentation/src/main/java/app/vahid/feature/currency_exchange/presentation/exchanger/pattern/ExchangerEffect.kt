67c5d00bd07fe5e24eddd8d8293aa94492fbd1fb
Vahid Moradi <zvm4zvm@gmail.com>
2023-01-09 15:11:23 +0330

    #4 Added possible exchanger effects and side effects

--- feature/currency_exchange/presentation/src/main/java/app/vahid/feature/currency_exchange/presentation/exchanger/pattern/ExchangerEffect.kt
+++ feature/currency_exchange/presentation/src/main/java/app/vahid/feature/currency_exchange/presentation/exchanger/pattern/ExchangerEffect.kt
@@ -2,12 +2,22 @@ package app.vahid.feature.currency_exchange.presentation.exchanger.pattern
 
 import app.vahid.common.presentation.error_handling.UiErrorType
 import app.vahid.common.presentation.pattern.Effect
+import app.vahid.domain.gateway.model.Balance
+import app.vahid.domain.gateway.model.CurrencyRate
 
 sealed interface ExchangerEffect : Effect, ExchangerEvent {
 
     data class OnLoadingStateChanged(val isLoading: Boolean) : ExchangerEffect
 
     data class OnErrorStateChanged(val uiErrorType: UiErrorType?) : ExchangerEffect
+
+    data class OnUpdateMyBalances(val balanceList: List<Balance>) : ExchangerEffect
+
+    data class OnUpdateOriginRates(val balanceList: List<CurrencyRate>) : ExchangerEffect
+
+    data class OnUpdateDestinationRates(val balanceList: List<CurrencyRate>) : ExchangerEffect
+
+    data class OnUpdateDestinationValue(val amount: Double) : ExchangerEffect
 
 }
 
